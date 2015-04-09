60de0eb90330b77dd4ee131572c1e225cbf90440
Justin Lennox <justinanthonylennox@gmail.com>
2015-04-09 13:33:30 -0400

    Initial Commit

---
+++ Wardrobe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wardrobe
+//
+//  Created by Justin Lennox on 4/9/15.
+//  Copyright (c) 2015 Justin Lennox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
